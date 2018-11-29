b2147ac9f05ecb279a47ae6e9670e89b87a2ed6e
snackura <45411730+snackura@users.noreply.github.com>
2018-11-28 17:19:51 -0800

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,19 +5,19 @@ $(document).ready(function() {
 	// making homepage navbar responsive
 	let startingPercent = 90
 
-	$("#arrow1").click(function() {
+	$('#arrow1').click(function() {
 		startingPercent = (startingPercent + 0.5)
 		while (startingPercent <= 105) {
 			startingPercent = (startingPercent + 0.5);
-			$("#homepage-navbar").css("top", startingPercent + "%");
+			$('#homepage-navbar').css('top', startingPercent + '%');
 		}
 
 	});
 
 
 	// arrow scroollll 
-	$("#arrow1").on("click", function(event) {
-   		if (this.hash !== "") {
+	$('#arrow1').on('click', function() {
+   		if (this.hash !== '') {
       		event.preventDefault();
       		var hash = this.hash;
 				$(window).animate({scrollTop: 700}, 800, function(){
@@ -45,31 +45,6 @@ $(document).ready(function() {
 
 	
 
-	// to get the scrollbars onto firefox and internet explorer
-		//shrug
-
-
-	// reviews page !!!
-	new TypeIt("#typeit", {
-		strings: ['<span class="title">Here are some reviews I have as an individual, that show that I am a hardworking and accomplished individual</span>', 
-		'<span class="quote">:)</span>',
-		'<span class="quote">5starts; I laughed so hard my innards fell out</span> <span class="source">-Daily Californian University News Editor Francesca Munsayac</span>',
-		'<span class="quote">You are a solid 3</span> <span class="source">- my older sister Sumer</span>',
-		'<span class="quote">You cook such good noodles that now I have standards for noodles</span> <span class="source">- my roommate</span>',
-		'<span class="quote">snac</span> <span class="source">- Daily Californian City News Editor Jessíca Jiménez</span>',
-		'<span class="quote">Please put on some pants</span> <span class="source">- my other roommate</span>',
-		'<span class="quote">hot thot tater tot</span> <span class="source">- my friend</span>',
-		],
-		speed: 60,
-		breakLines: false,
-		loop: true,
-		lifeLike: true,
-		pause: 500,
-		nextStringDelay: [1000,2000]
-	});
-
-	
-
 	// stories page sticky bar
 	$(window).scroll(function() {
 		if (document.body.scrollTop <= 70) {
@@ -81,4 +56,32 @@ $(document).ready(function() {
 		}
 
 	});
+
+
+	// i haven't made the comics page l0l
+	$(".speciallink").click(function() {
+		alert("I haven't made this page yet......lol I'm sorry");
+	});
+
+
+	// reviews page !!!
+	new TypeIt("#typeit", {
+		strings: ['<span class="quote">Here are some reviews I have received as an individual, showing that I am hardworking and accomplished</span>', 
+		'<span class="quote">Maybe :)</span>',
+		'<span class="quote">5starts; I laughed so hard my innards fell out</span> <span class="source">-Daily Californian University News Editor Francesca Munsayac</span>',
+		'<span class="quote">You are a solid 3</span> <span class="source">- my older sister Sumer</span>',
+		'<span class="quote">You cook such good noodles that now I have standards for noodles</span> <span class="source sarah">- my roommate</span>',
+		'<span class="quote">snac</span> <span class="source">- Daily Californian City News Editor Jessíca Jiménez</span>',
+		'<span class="quote">Please put on some pants</span> <span class="source">- my other roommate</span>',
+		'<span class="quote">hot thot tater tot</span> <span class="source">- my girlfriend</span>',
+		],
+		speed: 60,
+		breakLines: false,
+		loop: true,
+		lifeLike: true,
+		pause: 500,
+		nextStringDelay: [1000,2000]
+	});
+
+	
 });
